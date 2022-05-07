dd4456c22df1fa74ec514e00ad0d2a1b32ee6555
KevinBueno03 <kbueno@unah.hn>
2022-05-06 18:46:09 -0600

    adding map route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,18 +11,20 @@ import { UploadCurriculumComponent } from './components/upload-curriculum/upload
 import { UploadProfilePictureComponent } from './components/upload-profile-picture/upload-profile-picture.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { MapComponent } from './components/map/map.component';
 
 const routes: Routes = [
 
-  { path: '', redirectTo: 'doctor/login',pathMatch: 'full' },
-  { path: 'doctor/login', component: LoginComponent},
-  { path: 'doctor/register', component: RegisterComponent},
+  { path: '', redirectTo: 'doctor/login', pathMatch: 'full' },
+  { path: 'doctor/login', component: LoginComponent },
+  { path: 'doctor/register', component: RegisterComponent },
   { path: 'doctor/calendar', component: CalendarComponent, canActivate: [DoctorSegurityRouter] },
-  { path: 'doctor/dashboard', component: DashboardComponent,  canActivate: [DoctorSegurityRouter] },
+  { path: 'doctor/dashboard', component: DashboardComponent, canActivate: [DoctorSegurityRouter] },
   { path: 'doctor/upload-curriculum', component: UploadCurriculumComponent, canActivate: [DoctorSegurityRouter] },
   { path: 'doctor/upload-profile-picture', component: UploadProfilePictureComponent, canActivate: [DoctorSegurityRouter] },
-  { path: 'doctor/profile', component: ProfileComponent , canActivate: [DoctorSegurityRouter]},
+  { path: 'doctor/profile', component: ProfileComponent, canActivate: [DoctorSegurityRouter] },
   { path: 'doctor/edit-profile', component: EditProfileComponent, canActivate: [DoctorSegurityRouter] },
+  { path: 'doctor/map', component: MapComponent, canActivate: [DoctorSegurityRouter] },
 
 ];
 
